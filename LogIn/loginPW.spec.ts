fe400c842dc2cd697666c0cd4d8f81e4b505a8cb
Heet <18ME071.heet.nanda@vvpedulink.ac.in>
2026-03-18 18:38:39 +0530

    Add Credentials in separate file

--- LogIn/loginPW.spec.ts
+++ LogIn/loginPW.spec.ts
@@ -1,13 +1,17 @@
+import 'dotenv/config';
 import { test, expect } from '@playwright/test';
 
 test('User can login', async ({ page }) => {
+  const email = process.env.EMAIL;
+  const password = process.env.PASSWORD;
+
+  if (!email || !password) {
+    throw new Error('Missing EMAIL or PASSWORD in environment variables');
+  }
+
   await page.goto('https://php.inheritxdev.in/rugby_planner/qa/login');
 
-  // Email
-   await page.getByPlaceholder('Enter email').first().fill('julia.mason@yopmail.com');
-  await page.getByPlaceholder('Enter password').fill('YourPassword123');
+  await page.getByPlaceholder('Enter email').first().fill(email);
+  await page.getByPlaceholder('Enter password').fill(password);
   await page.getByRole('button', { name: 'Sign in' }).click();
-
-  // Optional: validate successful login (example)
-  // await expect(page).toHaveURL(/dashboard/);
 });
